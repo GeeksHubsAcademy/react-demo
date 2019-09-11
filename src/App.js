c2e1b184d8b3e1300f0e2c8aade424ac0c964509
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-09-11 17:55:36 +0200

    react demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      destinos: []
+    }
+  }
+  async componentDidMount() {
+    const destinos = await fetch('http://localhost:3001/destinations/').then(res => res.json())
+    this.setState({ destinos })
+  }
+  render() {
+    console.log('renderizado');
+    return (
+      <div className="App">
+        <Header />
+        <ul>
+          {this.state.destinos.map(destino => <li className="destino" key={destino.id}> {destino.travel} </li>)}
+        </ul>
+      </div>
+    )
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
-
 export default App;
